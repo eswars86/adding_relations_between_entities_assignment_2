278e1fd94be626029ad79e5017e71c1e450ccc2b
unknown <lakshmimadhavi37@gmail.com>
2025-04-19 11:40:51 +0530

    Completed assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,18 @@
-import React, { useState } from 'react';
-import ProductCard from './components/ProductCard';
-import './App.css';
 
-const initialProducts = [
-  {
-    id: 1,
-    name: "Wireless Headphones",
-    description: "High-quality sound with noise cancellation.",
-    image: "https://picsum.photos/300/200?random=1",
-    avgRating: 4.2,
-    totalRatings: 10
-  },
-  {
-    id: 2,
-    name: "Smartwatch",
-    description: "Track your fitness and notifications.",
-    image: "https://picsum.photos/300/200?random=2",
-    avgRating: 3.8,
-    totalRatings: 15
-  },
-  {
-    id: 3,
-    name: "Portable Speaker",
-    description: "Powerful sound in a compact design.",
-    image: "https://picsum.photos/300/200?random=3",
-    avgRating: 4.5,
-    totalRatings: 8
-  }
-];
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import AddMovie from './AddMovie/AddMovie';
 
-function App() {
-
- 
-
+const App = () => {
   return (
-    <div>
-     {/* code here */}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path='/addmovie' element={<AddMovie/>}/>
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
